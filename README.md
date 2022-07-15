af4698f3465a99904e7cc07d6710a9967614e7d3
abdalsalaam <abdalsalaamnafez@gmail.com>
2022-07-15 17:38:40 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,1 @@
+# woocommerce-promoted-product
---
+++ woocommerce-promoted-product.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Promoted Product
+ * Plugin URI: https://github.com/Abdalsalaam/woocommerce-promoted-product
+ * Description:
+ * Version: 0.1.0
+ * Author: Abdalsalaam Halawa
+ * Author URI: https://github.com/Abdalsalaam
+ * Text Domain: WCPP
+ * Domain Path: /languages/
+ *
+ */
