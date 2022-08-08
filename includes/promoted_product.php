82b090e2a3e6a99d90aa40ae4798c64f79cb97dd
abdalsalaam <abdalsalaamnafez@gmail.com>
2022-08-08 11:26:28 +0300

    duplicated code

--- includes/promoted_product.php
+++ includes/promoted_product.php
@@ -31,10 +31,8 @@ class promoted_product{
 		 */
 		add_action( 'wp_body_open', function (){
 			$promoted_product_data = $this->get_data();
-			$background_color = get_option( 'wcpp_settings_bg_color' );
-			$text_color = get_option( 'wcpp_settings_text_color' );
 			if(!is_null($promoted_product_data)){
-				echo '<div style="text-align: center; background: '.$background_color.';"><a style="color : '.$text_color.' ;" href="'.$promoted_product_data['link'].'">'.$promoted_product_data['title'].'</a></div>';
+				echo '<div style="text-align: center; background: '.$promoted_product_data['bg_color'].';"><a style="color : '.$promoted_product_data['text_color'].' ;" href="'.$promoted_product_data['link'].'">'.$promoted_product_data['title'].'</a></div>';
 			}
 		});
 	}
